8f5ef46132146d78378b6bb57b6baadb63cc153d
naetharu <naetharu@gmail.com>
2024-05-30 21:40:58 +0100

    add content support

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://main--naetharu-astro-blog-demo.netlify.app/",
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind()],
 });
