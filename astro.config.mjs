e6896a7a00383f34e8c2e741f6e29091d6a26087
naetharu <naetharu@gmail.com>
2024-07-24 13:10:05 +0100

    Add shape morphs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://main--naetharu-astro-blog-demo.netlify.app/",
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ["flubber"],
+    },
+  },
 });
